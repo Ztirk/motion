9acdd3c225d6012ef46024812bc7fb418d86b2d7
Kritz <tanakrit.promwungkwa@gmail.com>
2024-11-19 14:19:39 +0700

    idk

--- src/app/page.js
+++ src/app/page.js
@@ -1,28 +1,12 @@
 "use client";
 
 import { motion } from "motion/react";
-import { useRef } from "react";
 
 export default function Page() {
-  const boxElem = useRef();
-
-  const handleDragTarget = (e) => {
-    console.log(boxElem.current);
-    console.log(e.clientX);
-  };
-
   return (
-    <div
-      onDragOver={handleDragTarget}
-      className={`h-full
-     w-full border`}
-    >
-      <motion.div
-        ref={boxElem}
-        animate={{ x: 100 }}
-        draggable
-        className={`aspect-square border h-10 border-black bg-red-400`}
-      />
-    </div>
+    <motion.div
+      animate={{ x: 100, transition: { duration: 5 } }}
+      className="h-20 aspect-square bg-blue-400"
+    />
   );
 }
